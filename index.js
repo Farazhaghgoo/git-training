3d72bc592ea35fd99a7926a8979c423ce34502ca
faraz haghgoo <farazhaghgoo1@gmail.com>
2025-12-25 15:53:42 +0100

    feature1 v2

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log('Enjoy coding!');
 alert('Welcome to the JavaScript application!');
 console.log('This application is running successfully.');
 console.log('This is an additional log message.');
+
+age = prompt('enter your age:');
+console.log('User prompted for age input.');
